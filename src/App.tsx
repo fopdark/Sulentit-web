3bdc06cf25801f042590ec59a5eb9ae80d2a8d05
phz2712 <lhp2712@gmail.com>
2022-05-15 23:42:21 +0700

    feat/sulentit-2-web: add variable and function in scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React, { useEffect } from 'react';
-import './App.scss';
+import './styles/App.scss'
 import Aos from "aos";
 import "aos/dist/aos.css";
 import NavTab from './components/layout/NavTab';
-import HomePage from './components/screens/homepage';
-import Beyond from './components/screens/beyound';
-import Method from './components/screens/methods';
-import Environments from './components/screens/enviroment';
-import Inspiration from './components/screens/inspiration';
-import CenterAddresses from './components/screens/centerAddress';
-import Schedule from './components/screens/schedule';
-import Career from './components/screens/career';
-import LoginParent from './components/screens/loginParent';
-import LoginStudent from './components/screens/loginStudent';
 
 function App() {
     useEffect(() => {
